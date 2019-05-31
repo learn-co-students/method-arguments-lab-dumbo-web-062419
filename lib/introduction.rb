55cee4b32cc82c2b29f1dd4d904f036f52e47c89
github username <github email address>
2019-05-31 16:11:17 -0400

    defined methods with multiple arguments/parameters

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+def introduction(name)
+    puts "Hi, my name is #{name}.\n"
+end
+
+# Tests the method to see if it runs correctly
+introduction("Bobby")
+
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+introduction_with_language("name_inserted_here", "language_inserted_here")
+
